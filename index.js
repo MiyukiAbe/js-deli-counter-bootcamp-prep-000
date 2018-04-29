9419dd185e1f76ac4087eb73e669d9d323b37dde
Miyuki Abe <miyuki0809@gmail.com>
2018-04-29 17:31:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(katzDeliLine){
 
   if (katzDeliLine.length > 0) {
   katzDeliLine.shift(katzDeliLine[0]);   
-  return "Currently serving " + katzDeliLine[0] + ".";
+  return "Currently serving " + katzDeliLine.shift(katzDeliLine[0]) + ".";
 } else if (katzDeliLine.length === 0 ) {
   return "There is nobody waiting to be served!" 
 } 
